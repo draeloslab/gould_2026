35261dc545ffc357bf1ab2f9c7302fbf87f1d344
Jonathan Gould <jonathan.d.gould@gmail.com>
2026-08-31 16:24:38 -0400

    minor mantel test edits

--- figure_code/behavior/mantel_test.py
+++ figure_code/behavior/mantel_test.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.spatial.distance import squareform
 from scipy.stats import linregress
+from tqdm.auto import tqdm
 
 def get_residual(x, y):
     slope, intercept, r, p, se = linregress(x, y)
@@ -32,14 +33,15 @@ def single_fit(x, y, rng, confound_x=None, shuffle=False, precomputed_partial_x=
     return x, y, slope, intercept, r
 
 
-def mantel_test(x, y, rng, confound_x=None, n=1000):
+def mantel_test(x, y, rng, confound_x=None, n=1000, use_tqdm=False):
     """this function assumes the inputs have the same format as pdist output"""
     y_sq = squareform(y)
 
     precomputed_partial_x = get_residual(confound_x, x) if (confound_x is not None) else None
 
     test_statistic = single_fit(x, y_sq, rng, confound_x=confound_x, precomputed_partial_x=precomputed_partial_x, shuffle=False)[-1]
-    null_samples = [single_fit(x, y_sq, rng, confound_x=confound_x, precomputed_partial_x=precomputed_partial_x, shuffle=True)[-1] for _ in range(n-1)]
+    range_iterator = (tqdm(range(n-1)) if use_tqdm else range(n-1))
+    null_samples = [single_fit(x, y_sq, rng, confound_x=confound_x, precomputed_partial_x=precomputed_partial_x, shuffle=True)[-1] for _ in range_iterator]
 
     p = (np.abs(np.array([test_statistic] + null_samples)) >= np.abs(test_statistic)).mean()
 
